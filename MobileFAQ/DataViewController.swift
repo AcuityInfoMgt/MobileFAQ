894eb10c9a78e858f3be5f091da5f8e014547329
Kevin Ferrell <kferrell@gmail.com>
2014-11-28 17:36:18 -0500

    Add UI for mobile FAQ

--- MobileFAQ/DataViewController.swift
+++ MobileFAQ/DataViewController.swift
@@ -9,30 +9,70 @@
 import UIKit
 
 class DataViewController: UIViewController {
-
+    
     @IBOutlet weak var dataLabel: UILabel!
-    var dataObject: AnyObject?
-
-
+    @IBOutlet weak var containerView: UIView!
+    
+    var itemController: ModelController?
+    var itemIndex: Int = -1
+    var dataObject: Dictionary<String, String>?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Display the webview
+        var webView = UIWebView(frame: CGRectMake(0, 0, containerView.frame.width, containerView.frame.height))
+        webView.autoresizingMask = (UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight);
+        
+        if let obj: Dictionary<String, String> = dataObject {
+            let locationType = obj["LocationType"]
+            
+            if locationType == "local"
+            {
+                let filename: String = obj["URL"]!
+                let pathExtention = NSString(string: filename).pathExtension
+                let pathPrefix = NSString(string: filename).stringByDeletingPathExtension
+                
+                if let url = NSBundle.mainBundle().URLForResource(pathPrefix, withExtension: pathExtention) {
+                    webView.loadRequest(NSURLRequest(URL: url))
+                }
+                
+            } else if locationType == "remote" {
+                
+                let url = NSURL(string: obj["URL"]!)
+                webView.loadRequest(NSURLRequest(URL: url!))
+                
+            }
+            
+        }
+        
+        
+        
+        containerView.addSubview(webView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        if let obj: AnyObject = dataObject {
-            self.dataLabel!.text = obj.description
+        if let obj: Dictionary<String, String> = dataObject {
+            
+            let viewableIndex = itemIndex + 1
+            
+            if let controller = itemController {
+                dataLabel!.text = "Item \(viewableIndex) of \(controller.pageData.count)"
+            } else {
+                dataLabel!.text = "Item \(viewableIndex)"
+            }
+            
         } else {
-            self.dataLabel!.text = ""
+            dataLabel!.text = ""
         }
     }
-
-
+    
+    
 }
-
